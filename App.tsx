3af84952de117d5fe5f2be650ac6263ea19bffea
Aung Nanda Oo <aungnandaoo.425@gmail.com>
2025-03-03 20:32:25 -0800

    creating all kind of btn component using TypeSafe

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import Button from './components/Button';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Button title="Primary" size="sm"/>
+      <Button title="Primary" size="sm" variant="primary" loading={false}/>
+      <Button title="Secondary" size="sm" variant="outline" loading={false}/>
+      <Button title="Ghost" size="sm" variant="ghost" loading={false}/>
+      <Button title="Ghost" size="sm" variant="destructive"/>
+      <Button title="Ghost" size="sm" variant="ghost" loading={true}/>
     </SafeAreaView>
   );
 }
@@ -16,5 +19,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    gap: 20,
   },
 });
--- components/Button.tsx
+++ components/Button.tsx
@@ -1,11 +1,14 @@
-import { Pressable, SafeAreaView, Text } from 'react-native'
+import { Pressable, Text, ViewStyle, ActivityIndicator } from 'react-native'
 import React from 'react'
 
 type ButtonSize = "sm" | "md" | "lg";
+type ButtonVariant = "primary" | "outline" | "ghost" | "destructive";
 
 type ButtonProps = {
     title : string;
     size? : ButtonSize;
+    variant? : ButtonVariant;
+    loading? : boolean;
 }
 
 const sizeStyle : Record<ButtonSize, {height : number; padding : number; fontSize : number}> = {
@@ -14,24 +17,63 @@ const sizeStyle : Record<ButtonSize, {height : number; padding : number; fontSiz
     lg : {height : 55, fontSize : 18, padding: 20},
 }
 
-const Button = ({title, size='md'}:ButtonProps) => {
+const Button = ({title, size='md', variant="primary", loading=false}:ButtonProps) => {
+
+  function getVariantStyle(): ViewStyle {
+    switch(variant) {
+      case 'primary':
+        return {
+          backgroundColor: 'black',
+        };
+      case 'outline':
+        return {
+          backgroundColor: 'transparent',
+          borderWidth: 1,
+          borderColor: 'gray',
+        };
+      case 'ghost':
+        return {
+          backgroundColor: 'transparent',
+        };
+      case 'destructive':
+        return {
+          backgroundColor: 'red'
+        };
+      default:
+        return {
+          backgroundColor: 'transparent',
+        };
+    }
+  }  
+
+  function getTextColor(): string {
+    switch(variant) {
+      case 'primary':
+      case 'destructive':
+        return 'white';
+      default:
+        return 'black';
+    }
+  }
+
   return (
       <Pressable style={[
+          getVariantStyle(),
           {
               height : sizeStyle[size].height,
               borderRadius : 10,
               alignItems : 'center',
               justifyContent : 'center',
               paddingHorizontal : sizeStyle[size].padding,
-              backgroundColor : '#000',
+              opacity : loading ? 0.5 : 1, 
           }
       ]}>
         <Text style={[
           {
             fontSize : sizeStyle[size].fontSize,
-            color: "white"
+            color: getTextColor(),  
           }
-          ]}>{title}</Text>
+          ]}>{loading ? <ActivityIndicator/> : title}</Text>
     </Pressable>
   )
 }
